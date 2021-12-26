77af64ebfd160b9e6dda74d9ea6377962246b2df
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2021-12-26 10:19:24 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class get_pybind_include(object):
 ext_modules = [
     Extension(
         'vtsimc2',
-        ['vtsimc2.vtsimc2.cpp'],
+        ['vtsimc2.cpp'],
         include_dirs=[
             # Path to pybind11 headers
             get_pybind_include(),
